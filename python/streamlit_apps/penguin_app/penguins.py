60c4ec95b11727de4bb8d521d161c5767e7b9efb
ashkrit@gmail.com <Crack@it12>
2023-09-26 15:55:32 +0800

    Gender filter

--- python/streamlit_apps/penguin_app/penguins.py
+++ python/streamlit_apps/penguin_app/penguins.py
@@ -18,21 +18,30 @@ selected_x_var = st.selectbox('What do want the x variable to be?',
                               ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
 selected_y_var = st.selectbox('What about the y?',
                               ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
+selected_gender = st.selectbox('What gender do you want to filter for?',
+                               ['all penguins', 'male penguins', 'female penguins'])
 
 penguin_file = st.file_uploader("Select Your Local Penguins CSV (default provided)")
 if penguin_file is not None:
     penguins_df = pd.read_csv(penguin_file)
 else:
-    #penguins_df = pd.read_csv('../../data/penguins.csv')
-    st.stop() ## This allow to do flow control
+    penguins_df = pd.read_csv('../../data/penguins.csv')
+    #st.stop() ## This allow to do flow control
 
 # import our data
 st.write(penguins_df.head())
 
+if selected_gender=='male penguins':
+    penguins_df = penguins_df[penguins_df["sex"]=='male']
+elif selected_gender=='female penguins':    
+     penguins_df = penguins_df[penguins_df["sex"]=='female']
+else:
+    pass     
+
 
 #penguins_df = penguins_df[penguins_df['species'] == selected_species]
 alt_chart = (
-    alt.Chart(penguins_df, title=f"Scatterplot of Penguins")
+    alt.Chart(penguins_df, title=f"Scatterplot of {selected_gender} Penguins")
     .mark_circle()
     .encode(
         x=selected_x_var,

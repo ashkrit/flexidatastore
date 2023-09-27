74c3a79c8c4921c90e727edc11caa4331898c2f4
ashkrit@gmail.com <Crack@it12>
2023-09-27 13:37:03 +0800

    Add state management

--- python/app_streamlit.py
+++ python/app_streamlit.py
@@ -2,25 +2,39 @@ import streamlit as st
 import requests
 import pandas as pd
 import json
+import app_streamlit_api
 
 st.set_page_config(layout="wide")
 st.title("Ecommerce Admin Store")
 
-tab_query, tab_crud = st.tabs(["Query Results","Products, Customers and Orders"]);
+tab_query, tab_crud,tab_logs = st.tabs(["Query Results","Create Products, Customers and Orders","Logs - Show Activity"]);
 
-base_url="http://localhost"
+
+
+if 'log_messages' not in st.session_state:
+    st.session_state.log_messages = log_messages = {
+    "action":[],
+    "params":[],
+    "result":[],
+}
+
+if 'log_df' not in st.session_state:
+    st.session_state.log_df = pd.DataFrame(st.session_state.log_messages)
+
 
 row2_col1,row2_col2 = st.columns(2)
 
-def execute_insert_api(name:str, payload:str):
-    url = f"{base_url}/api/insert/{name}"
-    print(f"Sennding {payload} to {name}")
-    headers = {
-        'Content-Type': "application/json"
-    }
-    insert_result = requests.post(url,data=payload,headers=headers)
+def execute_insert(name:str, payload:str):
+    insert_result = app_streamlit_api.insert_object(name,payload)
     if(insert_result.status_code == 200):
         st.success(f"Insert {name} success")
+        
+        st.session_state.log_messages["action"].append("Insert")
+        st.session_state.log_messages["params"].append(f"{name} - {payload}")
+        st.session_state.log_messages["result"].append(f"Status {status_code}")
+
+        st.session_state.log_df = pd.DataFrame(st.session_state.log_messages)
+        
     else:
         st.error(f"Insert {insert_result} failed")
         st.stop()
@@ -30,9 +44,9 @@ with tab_crud:
     row1_col1, row1_col2,row1_col3 = st.columns(3)
     with row1_col1:
         st.write("#### Create Products, Customers and Orders")
-        object_name = st.text_input("Table Name")
+        object_name = st.selectbox("Insert into table", ["products","customers","orders"])
         payload_text = st.text_area("Payload")
-        st.button("Insert", key="insert_button", on_click=lambda: execute_insert_api(object_name, payload_text))
+        st.button("Insert", key="insert_button", on_click=lambda: execute_insert(object_name, payload_text))
         
     with row1_col2:
         st.write("#### Update Products, Customers and Orders")
@@ -41,29 +55,31 @@ with tab_crud:
 
 with tab_query:
     st.write("#### Show Products, Customers and Orders")
-    table_name = st.text_input("Search Products, Customers and Orders", key="search_query")
-   
+    table_name = st.selectbox("Search Table", ["products","customers","orders"])
 
     if len(table_name) > 0 :
         st.write(f"Showing result for {table_name}")
-        search_url = f"{base_url}/api/search/{table_name}"
-        search_result=requests.get(search_url)
-        if(search_result.status_code == 200):
-            table_data = pd.DataFrame(search_result.json())
-            
-            cols = table_data[0].map( lambda x : json.loads(x)).map(lambda x : x.keys()).drop_duplicates().to_list()
-            col_names =[cname for keys in cols for cname in keys]
-            for c in col_names:
-                if(table_data.columns.to_list().count(c) == 0):
-                    table_data[c] = table_data[0].map( lambda x : json.loads(x)).map(lambda x : x[c])
 
+        table_data,status_code =app_streamlit_api.search_object(table_name)
 
-            table_data.drop(0,axis="columns",inplace=True)
+        st.session_state.log_messages["action"].append("Search")
+        st.session_state.log_messages["params"].append(f"{table_name}")
+        st.session_state.log_messages["result"].append(f"Status {status_code} - {table_data.size} Rows")
 
+        st.session_state.log_df = pd.DataFrame(st.session_state.log_messages)
+        
+      
+        if(status_code == 200):
             st.write(table_data)
-            
         else:    
-            st.error(f"Server return error {search_result.status_code}")
+            st.error(f"Server return error {status_code}")
 
     else:
         st.stop()    
+
+
+
+with tab_logs:
+    st.write("#### Show users actions")
+    table = st.empty()
+    table.dataframe(st.session_state.log_df)
---
+++ python/app_streamlit_api.py
@@ -0,0 +1,43 @@
+import requests
+import json
+import pandas as pd
+
+
+
+base_url:str = "http://localhost"
+
+def insert_object(name: str, payload: str) -> requests.Response:
+    url = f"{base_url}/api/insert/{name}"
+    print(f"Sennding {payload} to {name}")
+    headers = {
+        'Content-Type': "application/json"
+    }
+    return requests.post(url, data=payload, headers=headers)
+
+def search_object(table_name: str) -> (pd.DataFrame,int):
+    search_url = f"{base_url}/api/search/{table_name}"
+    search_result=requests.get(search_url)
+    if(search_result.status_code == 200):
+        table_data = _create_dataframe(search_result)
+        return (table_data,search_result.status_code)
+    else:
+        return (pd.DataFrame(),search_result.status_code)
+
+def _create_dataframe(search_result):
+    table_data = pd.DataFrame(search_result.json())
+
+    col_names = _extract_cols_names(table_data)
+    _flat_columns(table_data, col_names)
+        
+    table_data.drop(0,axis="columns",inplace=True)
+    return table_data
+
+def _flat_columns(table_data, col_names):
+    for c in col_names:
+        if(table_data.columns.to_list().count(c) == 0):
+            table_data[c] = table_data[0].map( lambda x : json.loads(x)).map(lambda x : x[c])
+
+def _extract_cols_names(table_data:pd.DataFrame) -> list[str]:
+    cols = table_data[0].map( lambda x : json.loads(x)).map(lambda x : x.keys()).drop_duplicates().to_list()
+    col_names =[cname for keys in cols for cname in keys]
+    return col_names
